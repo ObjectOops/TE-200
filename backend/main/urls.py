6a471c5d963cf4e2a2bc328d5a7b56f0a5ed1dbb
Alex Yao <92550885+ObjectOops@users.noreply.github.com>
2024-10-14 16:05:05 +0000

    Routes.

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("instructor/create/", views.instructor_create, name="instructor create"),
+    path("instructor/class/<int:attendance_id>/", views.instructor_detail, name="instructor detail"),
+    path("student/join/", views.student_join, name="student join"),
+    path("student/class/<int:attendance_id>/", views.student_detail, name="student detail")
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+def instructor_create(request):
+    return HttpResponse("Instructor create.")
+
+def instructor_detail(request, attendance_id):
+    return HttpResponse(f"Instructor detail attendance {attendance_id}.")
+
+def student_join(request):
+    return HttpResponse("Student join.")
+
+def student_detail(request, attendance_id):
+    return HttpResponse(f"Student detail attendance {attendance_id}.")
