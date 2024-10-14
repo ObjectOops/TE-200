3cde7351de7a35faf8e882c419ef48a912e1e907
Alex Yao <92550885+ObjectOops@users.noreply.github.com>
2024-10-14 15:51:25 +0000

    Part 2.

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AttendanceCode, Attendance
+
+admin.site.register(AttendanceCode)
+admin.site.register(Attendance)
--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+class AttendanceCode(models.Model):
+    code = models.CharField(max_length=6)
+    opening = models.DateTimeField("opening time")
+    closing = models.DateTimeField("closing time")
+
+    def __str__(self):
+        return f"{self.code} | {self.opening} | {self.closing}"
+    
+    def is_open(self):
+        now = timezone.now()
+        return self.opening <= now and now < self.closing
+
+class Attendance(models.Model):
+    attendance_code = models.ForeignKey(AttendanceCode, on_delete=models.CASCADE)
+    date_taken = models.DateTimeField("date taken")
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.attendance_code.code} | {self.count}"
