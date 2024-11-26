c2f577046302df0bfdab23d3912ff2ae6a8d782d
Alex Yao <92550885+ObjectOops@users.noreply.github.com>
2024-11-26 21:39:16 +0000

    Remove temporary app.

--- te_200/settings.py
+++ te_200/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "main.apps.MainConfig"
+    "django.contrib.staticfiles"
 ]
 
 MIDDLEWARE = [
